0c8343e9aa6f9dceb3ba1af43cbb0d70a94700d9
MunishThakur <munishthakur.2008@gmail.com>
2016-08-16 11:17:57 +0530

    - Network Check class added having ability:   * can check connection type - wi-fi, network, no connection etc.   * can tell is network connected or not.

---
+++ app/src/main/java/com/superchat/utils/NetWork.java
@@ -0,0 +1,90 @@
+package com.superchat.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+/**
+ * Created by MotoBeans on 8/16/2016.
+ */
+
+public class NetWork {
+
+    public enum NetWorkTypes{
+        NO_CONNECTION,
+        WIFI,
+        MOBILE_NETWORK,
+        UNKNOWN
+    }
+
+    private static boolean connectionEstablished = false;
+
+    /**
+     * get the internet connection of the device
+     *
+     * @param context
+     * @return the type of internet connection
+     */
+    public static NetWorkTypes getNetwork(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+
+        if (info == null || !info.isConnected())
+            return NetWorkTypes.NO_CONNECTION;
+
+        if (info.getType() == ConnectivityManager.TYPE_WIFI)
+            return NetWorkTypes.WIFI;
+
+        if (info.getType() == ConnectivityManager.TYPE_MOBILE) {
+            int networkType = info.getSubtype();
+            switch (networkType) {
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN: // all above for 2G
+
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B:
+                case TelephonyManager.NETWORK_TYPE_EHRPD:
+                case TelephonyManager.NETWORK_TYPE_HSPAP: // all above for 3G
+
+                case TelephonyManager.NETWORK_TYPE_LTE:   // 4G
+                    return NetWorkTypes.MOBILE_NETWORK;
+                default:
+                    return NetWorkTypes.UNKNOWN;
+            }
+        }
+        return NetWorkTypes.UNKNOWN;
+    }
+
+    /**
+     * check if device is connected on the internet
+     *
+     * @param context
+     * @return true if is connected, false if isn't
+     */
+    public static boolean isConnected(Context context) {
+        NetWorkTypes netWorkType;
+        netWorkType = getNetwork(context);
+        if (netWorkType == NetWorkTypes.WIFI || netWorkType == NetWorkTypes.MOBILE_NETWORK) {
+            connectionEstablished = true;
+        } else {
+            connectionEstablished = false;
+        }
+        return connectionEstablished;
+    }
+
+    /**
+     * @return the boolean that tells if there is connection on the internet
+     */
+    public static boolean getConnectionEstablished() {
+        return connectionEstablished;
+    }
+}
